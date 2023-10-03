de7cc7bbd5aa924aa006781d8fd070b77a1271d5
luan <luan640@gmail.com>
2023-10-03 11:06:32 -0300

    outros

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, jsonify
+import psycopg2
+
+DB_HOST = "database-2.cdcogkfzajf0.us-east-1.rds.amazonaws.com"
+DB_NAME = "postgres"
+DB_USER = "postgres"
+DB_PASS = "15512332"
+
+conn = psycopg2.connect(dbname=DB_NAME, user=DB_USER,
+                        password=DB_PASS, host=DB_HOST)
+
+app = Flask(__name__)
+
+@app.route('/api/registros', methods=['GET'])
+def get_registros():
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM users')
+    registros = cursor.fetchall()
+    cursor.close()
+    return jsonify(registros)
+
+if __name__ == '__main__':
+    app.run(debug=True)
